98af14c9cef1d76a3aa1cff7ba62701fede70fad
Aleksey Bykhun <theisease@gmail.com>
2022-09-06 17:58:09 +0200

    add task script

--- test/project.test.ts
+++ test/project.test.ts
@@ -1,8 +1,9 @@
 // tslint:disable-next-line no-implicit-dependencies
 import { assert } from "chai";
-import path from "path";
+// import path from "path";
+// import { TaskDefinition } from "hardhat/types";
 
-import { ExampleHardhatRuntimeEnvironmentField } from "../src/ExampleHardhatRuntimeEnvironmentField";
+// import { ExampleHardhatRuntimeEnvironmentField } from "../src/ExampleHardhatRuntimeEnvironmentField";}
 
 import { useEnvironment } from "./helpers";
 
@@ -10,37 +11,46 @@ describe("Integration tests examples", function () {
   describe("Hardhat Runtime Environment extension", function () {
     useEnvironment("hardhat-project");
 
-    it("Should add the example field", function () {
-      assert.instanceOf(
-        this.hre.example,
-        ExampleHardhatRuntimeEnvironmentField
+    it("Should add the upload task", function () {
+      assert.exists(
+        this.hre.tasks.upload
       );
     });
 
-    it("The example filed should say hello", function () {
-      assert.equal(this.hre.example.sayHello(), "hello");
+    it("Should have action setup", function () {
+      assert.exists(this.hre.tasks.upload.action);
     });
   });
 
   describe("HardhatConfig extension", function () {
     useEnvironment("hardhat-project");
 
-    it("Should add the newPath to the config", function () {
-      assert.equal(
-        this.hre.config.paths.newPath,
-        path.join(process.cwd(), "asd")
-      );
-    });
+    // xit("Should add the newPath to the config", function () {
+    //   assert.equal(
+    //     this.hre.config.paths.newPath,
+    //     path.join(process.cwd(), "asd")
+    //   );
+    // });
   });
 });
 
 describe("Unit tests examples", function () {
   describe("ExampleHardhatRuntimeEnvironmentField", function () {
-    describe("sayHello", function () {
+    useEnvironment("hardhat-project");
+
+    describe("Task", function () {
       it("Should say hello", function () {
-        const field = new ExampleHardhatRuntimeEnvironmentField();
-        assert.equal(field.sayHello(), "hello");
+        const task = this.hre.tasks.upload;
+
+        assert.equal(task.name, "upload");
+        assert.equal(task.description, "Uploads a compiled contract to IPFS and returns deploy link");
+
+        assert.exists(task.paramDefinitions.args);
+        assert.exists(task.paramDefinitions.ascii);
+
+        assert.equal(task.positionalParamDefinitions[0].name, "contract");
       });
+
     });
   });
 });
