15ddbd0c64c443962678e939dfe1b38206bfc50e
Aleksey Bykhun <theisease@gmail.com>
2022-09-06 18:21:05 +0200

    edit tab width

--- test/fixture-projects/hardhat-project/hardhat.config.ts
+++ test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -4,8 +4,8 @@ import { HardhatUserConfig } from "hardhat/types";
 import "../../../src/index";
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
-  defaultNetwork: "hardhat",
+    solidity: "0.7.3",
+    defaultNetwork: "hardhat",
 };
 
 export default config;
--- test/project.test.ts
+++ test/project.test.ts
@@ -8,49 +8,52 @@ import { assert } from "chai";
 import { useEnvironment } from "./helpers";
 
 describe("Integration tests examples", function () {
-  describe("Hardhat Runtime Environment extension", function () {
-    useEnvironment("hardhat-project");
+    describe("Hardhat Runtime Environment extension", function () {
+        useEnvironment("hardhat-project");
 
-    it("Should add the upload task", function () {
-      assert.exists(this.hre.tasks.upload);
+        it("Should add the upload task", function () {
+            assert.exists(this.hre.tasks.upload);
+        });
+
+        it("Should have action setup", function () {
+            assert.exists(this.hre.tasks.upload.action);
+        });
     });
 
-    it("Should have action setup", function () {
-      assert.exists(this.hre.tasks.upload.action);
+    describe("HardhatConfig extension", function () {
+        useEnvironment("hardhat-project");
+
+        // xit("Should add the newPath to the config", function () {
+        //   assert.equal(
+        //     this.hre.config.paths.newPath,
+        //     path.join(process.cwd(), "asd")
+        //   );
+        // });
     });
-  });
-
-  describe("HardhatConfig extension", function () {
-    useEnvironment("hardhat-project");
-
-    // xit("Should add the newPath to the config", function () {
-    //   assert.equal(
-    //     this.hre.config.paths.newPath,
-    //     path.join(process.cwd(), "asd")
-    //   );
-    // });
-  });
 });
 
 describe("Unit tests examples", function () {
-  describe("ExampleHardhatRuntimeEnvironmentField", function () {
-    useEnvironment("hardhat-project");
+    describe("ExampleHardhatRuntimeEnvironmentField", function () {
+        useEnvironment("hardhat-project");
 
-    describe("Task", function () {
-      it("Should say hello", function () {
-        const task = this.hre.tasks.upload;
+        describe("Task", function () {
+            it("Should say hello", function () {
+                const task = this.hre.tasks.upload;
 
-        assert.equal(task.name, "upload");
-        assert.equal(
-          task.description,
-          "Uploads a compiled contract to IPFS and returns deploy link"
-        );
+                assert.equal(task.name, "upload");
+                assert.equal(
+                    task.description,
+                    "Uploads a compiled contract to IPFS and returns deploy link"
+                );
 
-        assert.exists(task.paramDefinitions.args);
-        assert.exists(task.paramDefinitions.ascii);
+                assert.exists(task.paramDefinitions.args);
+                assert.exists(task.paramDefinitions.ascii);
 
-        assert.equal(task.positionalParamDefinitions[0].name, "contract");
-      });
+                assert.equal(
+                    task.positionalParamDefinitions[0].name,
+                    "contract"
+                );
+            });
+        });
     });
-  });
 });
